bbf690b09a429d4d8cd18a26ead305077d7eab61
Sebastian Pitur <seba_pitur@yahoo.com>
2025-04-05 15:30:02 +0300

    update visualisation

--- results/pretty_print_results.py
+++ results/pretty_print_results.py
@@ -1,6 +1,13 @@
 import pandas as pd
 from tabulate import tabulate
 
+
+def display_by_metric(df: pd.DataFrame):
+    for metric in df['metric'].unique():
+        print(f"################# {metric} #####################")
+        print(tabulate(df[df['metric']==metric], headers='keys', tablefmt='pretty'))
+
+
 # Sample DataFrame
 routing_df = pd.read_csv("comparison_routing.csv")
 dissemination_df = pd.read_csv("comparison_dissemination.csv")
@@ -9,20 +16,27 @@ dissemination_df = pd.read_csv("comparison_dissemination.csv")
 print()
 print("#################ROUTING EPIDEMIC###################")
 print()
-print(tabulate(routing_df[routing_df['comparison_algo']=='EPIDEMIC'], headers='keys', tablefmt='pretty'))
+
+epidemic_routing = routing_df[routing_df['comparison_algo']=='EPIDEMIC']
+display_by_metric(epidemic_routing)
 
 print()
 print("#################ROUTING SPRAY FOCUS###################")
 print()
-print(tabulate(routing_df[routing_df['comparison_algo']=='SPRAY_FOCUS'], headers='keys', tablefmt='pretty'))
 
+spray_routing = routing_df[routing_df['comparison_algo']=='SPRAY_FOCUS']
+display_by_metric(spray_routing)
 
 
 print()
 print("#################DISSEMINATION EPIDEMIC###################")
 print()
-print(tabulate(dissemination_df[dissemination_df['comparison_algo']=='EPIDEMIC'], headers='keys', tablefmt='pretty'))
+epidemic_dissemination = dissemination_df[dissemination_df['comparison_algo']=='EPIDEMIC']
+display_by_metric(epidemic_dissemination)
+
+
 print()
 print("#################DISSEMINATION SPRAY FOCUS###################")
 print()
-print(tabulate(dissemination_df[dissemination_df['comparison_algo']=='SPRAY_FOCUS'], headers='keys', tablefmt='pretty'))
+spray_dissemination = dissemination_df[dissemination_df['comparison_algo']=='SPRAY_FOCUS'] 
+display_by_metric(spray_dissemination)
